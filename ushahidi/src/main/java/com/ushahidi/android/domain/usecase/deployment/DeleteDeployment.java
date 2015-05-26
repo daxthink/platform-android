3b898dc99d27e254e2ecf84e3de37217285cd922
Henry Addo <addhen@linuxmail.org>
2015-05-27 07:27:27 +0900

    Implement deployment deletion use case

---
+++ ushahidi/src/main/java/com/ushahidi/android/domain/usecase/deployment/DeleteDeployment.java
@@ -0,0 +1,42 @@
+package com.ushahidi.android.domain.usecase.deployment;
+
+import com.addhen.android.raiburari.domain.executor.PostExecutionThread;
+import com.addhen.android.raiburari.domain.executor.ThreadExecutor;
+import com.addhen.android.raiburari.domain.usecase.Usecase;
+import com.ushahidi.android.domain.entity.Deployment;
+import com.ushahidi.android.domain.repository.DeploymentRepository;
+
+import javax.inject.Inject;
+
+import rx.Observable;
+
+/**
+ * Use case that deletes a {@link Deployment} from the local database.
+ *
+ * @author Ushahidi Team <team@ushahidi.com>
+ */
+public class DeleteDeployment extends Usecase {
+
+    private final DeploymentRepository mDeploymentRepository;
+
+    private Long mDeploymentId;
+
+    @Inject
+    protected DeleteDeployment(DeploymentRepository deploymentRepository,
+            ThreadExecutor threadExecutor, PostExecutionThread postExecutionThread) {
+        super(threadExecutor, postExecutionThread);
+        mDeploymentRepository = deploymentRepository;
+    }
+
+    public void setDeploymentId(Long deploymentId) {
+        mDeploymentId = deploymentId;
+    }
+
+    @Override
+    protected Observable buildUseCaseObservable() {
+        if (mDeploymentId == null) {
+            throw new RuntimeException("Deployment ID is null. You must call setDeployment(...)");
+        }
+        return mDeploymentRepository.deleteEntity(mDeploymentId);
+    }
+}
