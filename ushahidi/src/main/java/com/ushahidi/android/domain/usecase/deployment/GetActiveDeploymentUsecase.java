e2017fc9dfd9cdf854cf7b4e1fdb9dfb1290d776
Henry Addo <addhen@linuxmail.org>
2015-05-27 21:31:26 +0900

    Implement get active deployment use case

---
+++ ushahidi/src/main/java/com/ushahidi/android/domain/usecase/deployment/GetActiveDeploymentUsecase.java
@@ -0,0 +1,34 @@
+package com.ushahidi.android.domain.usecase.deployment;
+
+import com.addhen.android.raiburari.domain.executor.PostExecutionThread;
+import com.addhen.android.raiburari.domain.executor.ThreadExecutor;
+import com.addhen.android.raiburari.domain.usecase.Usecase;
+import com.ushahidi.android.domain.entity.Deployment;
+import com.ushahidi.android.domain.repository.DeploymentRepository;
+
+import javax.inject.Inject;
+
+import rx.Observable;
+
+/**
+ * Use case for getting an active deployment.
+ *
+ * @author Ushahidi Team <team@ushahidi.com>
+ */
+public class GetActiveDeploymentUsecase extends Usecase {
+
+    private final DeploymentRepository mDeploymentRepository;
+
+    @Inject
+    protected GetActiveDeploymentUsecase(DeploymentRepository deploymentRepository,
+            ThreadExecutor threadExecutor,
+            PostExecutionThread postExecutionThread) {
+        super(threadExecutor, postExecutionThread);
+        mDeploymentRepository = deploymentRepository;
+    }
+
+    @Override
+    protected Observable buildUseCaseObservable() {
+        return mDeploymentRepository.getByStatus(Deployment.Status.ACTIVATED);
+    }
+}
---
+++ ushahidi/src/test/java/com/ushahidi/android/domain/usecase/deployment/GetActiveDeploymentUsecaseTest.java
@@ -0,0 +1,57 @@
+package com.ushahidi.android.domain.usecase.deployment;
+
+import com.addhen.android.raiburari.domain.executor.PostExecutionThread;
+import com.addhen.android.raiburari.domain.executor.ThreadExecutor;
+import com.ushahidi.android.BuildConfig;
+import com.ushahidi.android.domain.entity.Deployment;
+import com.ushahidi.android.domain.repository.DeploymentRepository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+/**
+ * Tests {@link GetActiveDeploymentUsecase}
+ *
+ * @author Ushahidi Team <team@ushahidi.com>
+ */
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(sdk = 21, constants = BuildConfig.class)
+public class GetActiveDeploymentUsecaseTest {
+
+    @Mock
+    private ThreadExecutor mockThreadExecutor;
+
+    @Mock
+    private PostExecutionThread mockPostExecutionThread;
+
+    @Mock
+    private DeploymentRepository mockDeploymentRepository;
+
+    private GetActiveDeploymentUsecase mGetActiveDeploymentUsecase;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        mGetActiveDeploymentUsecase = new GetActiveDeploymentUsecase(
+                mockDeploymentRepository, mockThreadExecutor, mockPostExecutionThread);
+    }
+
+    @Test
+    public void shouldSuccessfullyDeleteDeployment() {
+        mGetActiveDeploymentUsecase.buildUseCaseObservable();
+        verify(mockDeploymentRepository).getByStatus(Deployment.Status.ACTIVATED);
+
+        verifyNoMoreInteractions(mockDeploymentRepository);
+        verifyZeroInteractions(mockPostExecutionThread);
+        verifyZeroInteractions(mockThreadExecutor);
+    }
+}
