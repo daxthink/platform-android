025513e144d4210b21264ff43823c7789e0831d1
Henry Addo <addhen@linuxmail.org>
2015-06-02 23:00:04 +0900

    Implements user api

---
+++ ushahidi/src/main/java/com/ushahidi/android/data/api/UserApi.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2015 Ushahidi Inc
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU Affero General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program in the file LICENSE-AGPL. If not, see
+ *  https://www.gnu.org/licenses/agpl-3.0.html
+ */
+
+package com.ushahidi.android.data.api;
+
+import com.ushahidi.android.data.api.auth.AccessToken;
+import com.ushahidi.android.data.api.auth.Payload;
+import com.ushahidi.android.data.api.service.UserService;
+import com.ushahidi.android.data.entity.UserEntity;
+import com.ushahidi.android.data.exception.NetworkConnectionException;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import javax.inject.Inject;
+
+import rx.Observable;
+
+/**
+ * User API
+ *
+ * @author Ushahidi Team <team@ushahidi.com>
+ */
+public class UserApi {
+
+    private final Context mContext;
+
+    private final UserService mUserService;
+
+    @Inject
+    public UserApi(@NonNull Context context, @NonNull UserService userService) {
+        mContext = context;
+        mUserService = userService;
+    }
+
+    public Observable<AccessToken> loginUserAccount(@NonNull Payload payload) {
+        return Observable.create(subscriber -> {
+            if (isDeviceConnectedToInternet(mContext)) {
+                mUserService.getAccessToken(payload);
+            } else {
+                subscriber.onError(new NetworkConnectionException());
+            }
+        });
+    }
+
+    public Observable<UserEntity> getUserProfile() {
+        return Observable.create(subscriber -> {
+            if (isDeviceConnectedToInternet(mContext)) {
+                mUserService.getUser();
+            } else {
+                subscriber.onError(new NetworkConnectionException());
+            }
+        });
+    }
+
+    // Workaround for testing static methods
+    public boolean isDeviceConnectedToInternet(Context context) {
+        return ApiUtil.isDeviceConnectedToInternet(context);
+    }
+}
