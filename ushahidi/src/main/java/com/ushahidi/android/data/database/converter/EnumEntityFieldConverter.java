69a993cffd24e757715529a776cba59930c35f21
Henry Addo <addhen@linuxmail.org>
2015-06-03 12:58:32 +0900

    Implement converter for enum fields

--- ushahidi/src/main/java/com/ushahidi/android/data/database/converter/EnumEntityFieldConverter.java
+++ ushahidi/src/main/java/com/ushahidi/android/data/database/converter/EnumEntityFieldConverter.java
@@ -17,9 +17,41 @@
 
 package com.ushahidi.android.data.database.converter;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import java.util.Locale;
+
+import nl.qbusict.cupboard.convert.EntityConverter;
+import nl.qbusict.cupboard.convert.FieldConverter;
+
 /**
+ * Converts entity enum fields so it's value is saved in the table
+ *
  * @author Ushahidi Team <team@ushahidi.com>
  */
-public class EnumEntityFieldConverter {
+public class EnumEntityFieldConverter<E extends Enum> implements FieldConverter<E> {
+
+    private final Class<E> mEnumClass;
+
+    public EnumEntityFieldConverter(Class<E> enumClass) {
+        this.mEnumClass = enumClass;
+    }
+
+    @Override
+    public E fromCursorValue(Cursor cursor, int columnIndex) {
+        return (E) Enum.valueOf(mEnumClass, cursor.getString(columnIndex).toUpperCase(
+                Locale.getDefault()));
+    }
+
+    @Override
+    public void toContentValue(E value, String key, ContentValues values) {
+        values.put(key, value.toString());
+    }
+
+    @Override
+    public EntityConverter.ColumnType getColumnType() {
+        return EntityConverter.ColumnType.TEXT;
+    }
 
 }
