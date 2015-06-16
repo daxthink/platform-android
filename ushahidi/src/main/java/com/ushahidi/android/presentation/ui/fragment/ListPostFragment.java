97a9091e145e7ce75b189de4767d9b4119753880
Henry Addo <addhen@linuxmail.org>
2015-06-16 17:15:13 +0900

    Add post listing layout

---
+++ ushahidi/src/main/java/com/ushahidi/android/presentation/ui/fragment/ListPostFragment.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2015 Ushahidi Inc
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU Affero General Public License as published by the Free
+ *  Software Foundation, either version 3 of the License, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program in the file LICENSE-AGPL. If not, see
+ *  https://www.gnu.org/licenses/agpl-3.0.html
+ */
+
+package com.ushahidi.android.presentation.ui.fragment;
+
+import com.addhen.android.raiburari.presentation.ui.adapter.BaseRecyclerViewAdapter;
+import com.addhen.android.raiburari.presentation.ui.fragment.BaseFragment;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * @author Ushahidi Team <team@ushahidi.com>
+ */
+public class ListPostFragment extends BaseRecyclerViewAdapter {
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public int getAdapterItemCount() {
+        return 0;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+}
