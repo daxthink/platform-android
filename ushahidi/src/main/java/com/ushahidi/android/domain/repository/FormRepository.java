6cb885b90bfcd97c71864e54dd5f1250bb90a642
Henry Addo <addhen@linuxmail.org>
2015-08-13 15:41:17 -0400

    Remove unused import

--- ushahidi/src/main/java/com/ushahidi/android/domain/repository/FormRepository.java
+++ ushahidi/src/main/java/com/ushahidi/android/domain/repository/FormRepository.java
@@ -2,7 +2,6 @@ package com.ushahidi.android.domain.repository;
 
 import com.ushahidi.android.domain.entity.Form;
 import com.ushahidi.android.domain.entity.From;
-import com.ushahidi.android.domain.entity.GeoJson;
 
 import rx.Observable;
 
@@ -14,7 +13,7 @@ import rx.Observable;
 public interface FormRepository {
 
     /**
-     * Get a list of {@link GeoJson} from either the database or online.
+     * Get a list of {@link Form} from either the database or online.
      *
      * @param deploymentId An ID of {@link com.ushahidi.android.domain.entity.Deployment}
      * @param from         Where to get the form from. Either from local storage or online
