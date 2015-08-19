8d4bca7871b642cf59f1d343021397d1243217e1
Henry Addo <addhen@linuxmail.org>
2015-08-19 13:58:39 -0400

    Add repo for form attribute

---
+++ ushahidi/src/main/java/com/ushahidi/android/domain/repository/FormAttributeRepository.java
@@ -0,0 +1,31 @@
+package com.ushahidi.android.domain.repository;
+
+import com.ushahidi.android.domain.entity.FormAttribute;
+import com.ushahidi.android.domain.entity.From;
+
+import java.util.List;
+
+import rx.Observable;
+
+/**
+ * @author Ushahidi Team <team@ushahidi.com>
+ */
+public interface FormAttributeRepository {
+
+    /**
+     * Get a list of {@link FormAttribute} from either the database or online.
+     *
+     * @param deploymentId An ID of {@link com.ushahidi.android.domain.entity.Deployment}
+     * @param from         Where to get the formattribute from. Either from local storage or online
+     * @return The retrieved form attribute.
+     */
+    Observable<List<FormAttribute>> getForms(Long deploymentId, From from);
+
+    /**
+     * Add/Update a {@link FormAttribute}.
+     *
+     * @param form The FormAttribute to be saved.
+     * @return The row affected
+     */
+    Observable<Long> putForm(FormAttribute form);
+}
