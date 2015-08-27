1df561232926c5ab9407a1da991f2834a0a676c8
Henry Addo <addhen@linuxmail.org>
2015-08-28 08:11:44 +0900

    Make data entity abstract so it should be extended and not instantiated

--- ushahidi/src/main/java/com/ushahidi/android/data/entity/Data.java
+++ ushahidi/src/main/java/com/ushahidi/android/data/entity/Data.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 /**
  * @author Ushahidi Team <team@ushahidi.com>
  */
-public class Data {
+public abstract class Data {
 
     @SerializedName("id")
     public Long _id;
