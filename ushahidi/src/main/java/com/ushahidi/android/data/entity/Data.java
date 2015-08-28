d9bf0fdde78b2d41a8967831c4c88d0b9d9c4a7e
Henry Addo <addhen@linuxmail.org>
2015-08-28 10:30:13 +0900

    Add doc for id field for data entity

--- ushahidi/src/main/java/com/ushahidi/android/data/entity/Data.java
+++ ushahidi/src/main/java/com/ushahidi/android/data/entity/Data.java
@@ -7,6 +7,11 @@ import com.google.gson.annotations.SerializedName;
  */
 public abstract class Data {
 
+    /**
+     * The id field has an underscore to ensure any existing id value will be
+     * replaced. if _id is null then a new entity will be created. This is {@link cupboard()} way
+     * of avoiding duplicated IDs
+     */
     @SerializedName("id")
     public Long _id;
 }
